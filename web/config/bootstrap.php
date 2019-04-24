5732d00dc647e9846635875d0c85cb957fd695af
ariany-f <ariany_f@hotmail.com>
2019-04-24 13:30:43 -0300

    headers

--- web/config/bootstrap.php
+++ web/config/bootstrap.php
@@ -61,7 +61,9 @@ use Cake\Utility\Security;
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, GET, PUT, PATCH, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: *');
-
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    exit(0);
+}
 /*
  * Read configuration file and inject configuration into various
  * CakePHP classes.
