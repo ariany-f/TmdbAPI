8cff404f4271e4a71dd152b45a2c90ff5aec1632
ariany-f <ariany_f@hotmail.com>
2019-04-25 15:41:37 -0300

    header

--- web/config/bootstrap.php
+++ web/config/bootstrap.php
@@ -69,12 +69,9 @@ use Cake\Utility\Security;
 
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, GET, PUT, PATCH, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method');
 header('Access-Control-Allow-Credentials: true');
 header('Accept: application/json');
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    exit(0);
-}
 
 try {
     Configure::config('default', new PhpConfig());
