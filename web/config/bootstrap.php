d04107242c49278c92eb695d2f983a00b5cdf97d
ariany-f <ariany_f@hotmail.com>
2019-04-25 15:47:46 -0300

    header

--- web/config/bootstrap.php
+++ web/config/bootstrap.php
@@ -72,6 +72,9 @@ header('Access-Control-Allow-Methods: POST, GET, PUT, PATCH, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method');
 header('Access-Control-Allow-Credentials: true');
 header('Accept: application/json');
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    exit(0);
+}
 
 try {
     Configure::config('default', new PhpConfig());
