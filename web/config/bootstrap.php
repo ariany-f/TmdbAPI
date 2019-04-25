44734e47eb013e96303d8dc4f0a887417b70f249
ariany-f <ariany_f@hotmail.com>
2019-04-25 10:24:52 -0300

    headers

--- web/config/bootstrap.php
+++ web/config/bootstrap.php
@@ -61,6 +61,7 @@ use Cake\Utility\Security;
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, GET, PUT, PATCH, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: *');
+
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     exit(0);
 }
