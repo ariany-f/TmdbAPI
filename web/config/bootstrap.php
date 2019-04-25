3f6c3fa0bf2707f1c825a29ab1dbb589db65fe31
ariany-f <ariany_f@hotmail.com>
2019-04-25 11:26:16 -0300

    headers

--- web/config/bootstrap.php
+++ web/config/bootstrap.php
@@ -66,14 +66,6 @@ use Cake\Utility\Security;
  * that changes from configuration that does not. This makes deployment simpler.
  */
 
-
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: POST, GET, PUT, PATCH, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: *');
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    exit(0);
-}
-
 try {
     Configure::config('default', new PhpConfig());
     Configure::load('app', 'default', false);
