dcabfe92f6210d434dd4b4c33ccba37c8c0ad866
ariany-f <ariany_f@hotmail.com>
2019-05-28 12:02:23 -0300

    video key not id

--- web/src/Controller/MoviesController.php
+++ web/src/Controller/MoviesController.php
@@ -523,7 +523,7 @@ class MoviesController extends AppController
 
         foreach($result['results'] as $i => $video) {
             if($video['site'] == "YouTube") {
-                $result['results'][$i]['url'] = 'https://www.youtube.com/watch?v=' . $video['id'];
+                $result['results'][$i]['url'] = 'https://www.youtube.com/watch?v=' . $video['SUXWAEX2jlg'];
             }
             else {
                 $result['results'][$i]['url'] = null;
